dbf56912b58077ce3adbfb42659bdb1cc9f441fa
N8411 <141395231+N8411@users.noreply.github.com>
2026-03-19 10:41:06 +0800

    Create Student class for student profiles
    Added Student class to represent student profiles in SLMS.

---
+++ Student.java
@@ -0,0 +1,21 @@
+/**
+ * Represents a student profile within the Student Learning Management System (SLMS).
+ * 
+ * This class stores essential student details such as the student's first and last name, 
+ * student ID, email, and phone number. It provides getter and setter methods to access 
+ * and modify these attributes, with the exception of the student ID, which is set upon 
+ * creation and cannot be edited later.
+ * 
+ */
+public class Student {
+    // private data members (wan)
+
+    // Student Constructor (wan)
+
+    // Getter methods (asyraaf)
+
+    // Setter methods (asyraaf)
+
+    // display student information (Irfan)
+
+}
