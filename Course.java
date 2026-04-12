7b6b69c07cf5d1de0067ac7426ab8f4fdeda00b4
N8411 <141395231+N8411@users.noreply.github.com>
2026-04-12 18:42:36 +0800

    Add courseType to Course constructor and methods

--- Course.java
+++ Course.java
@@ -11,14 +11,16 @@ public class Course {
     // private data members
     private String courseName;
     private String courseCode; // No setter provided so it cannot be edited
+     private String courseType; // core, elective, university
     private int creditHours;
     private String summary;
     private String msTeamsLink;
 
     // Course Constructor
-    public Course(String name, String code, int creditHours, String summary, String msTeamsLink) {
+    public Course(String name, String code, String courseType, int creditHours, String summary, String msTeamsLink) {
         this.courseName = name;
         this.courseCode = code;
+        this.courseType = courseType;
         this.creditHours = creditHours;
         this.summary = summary;
         this.msTeamsLink = msTeamsLink;
@@ -27,19 +29,21 @@ public class Course {
     // Getter methods
     public String getCourseName() { return courseName; }
     public String getCourseCode() { return courseCode; }
+    public String getCourseType() { return courseType; }
     public int getCreditHours() { return creditHours; }
     public String getSummary() { return summary; }
     public String getMsTeamsLink() { return msTeamsLink; }
 
     // Setter methods
     public void setCourseName(String courseName) { this.courseName = courseName; }
+    public void setCourseType(String courseType) { this.courseType = courseType; }
     public void setCreditHours(int creditHours) { this.creditHours = creditHours; }
     public void setSummary(String summary) { this.summary = summary; }
     public void setMsTeamsLink(String msTeamsLink) { this.msTeamsLink = msTeamsLink; }
 
     // Display course information
     public void displayCourse() {
-        System.out.println("Code: " + courseCode + " | Name: " + courseName + " | Credits: " + creditHours);
+        System.out.println("Code: " + courseCode + " | Name: " + courseName + " | Type: " + courseType + " | Credits: " + creditHours);
         System.out.println("Summary: " + summary);
         System.out.println("Teams Link: " + msTeamsLink + "\n");
     }
